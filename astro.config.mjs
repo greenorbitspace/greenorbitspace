7c5127288d9afdb61b2b42537686c9d098fe6f54
rjmlaird <rjmlaird@gmail.com>
2025-12-08 15:11:05 +0000

    greenorbitspace

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://greenorbitspace.github.io',
-  base: '/greenorbitspace',
+  base: '/',
   integrations: [
     alpinejs(),
     react({ fastRefresh: true }),
